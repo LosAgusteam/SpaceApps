3da0ec3e8793ed86a1f2ec3b5051bb929e4df930
Guille <guillermobernallou@gmail.com>
2024-10-05 12:41:50 +0200

    Muchos cambios

--- Scripts/SystemHandler.gd
+++ Scripts/SystemHandler.gd
@@ -13,6 +13,70 @@ var loading_thread: Thread
 # MATERIALS #
 var star_shader = preload("res://Materials/StarShader.tres")
 
+# SOUNDS # 
+var active_sounds = 0
+var max_sounds = 15
+var sounds_array = ["oldusty", "ooze", "keys", "strings", "ritmicos"];
+
+func calculate_pitch(mass: float, min_mass: float = 0.000001, max_mass: float = 1.0) -> int:
+	# Limitar la masa para que no salga del rango definido
+	mass = max(min_mass, min(mass, max_mass))
+	
+	# Calcular el valor inversamente proporcional entre 1 y 28
+	# Queremos que la masa mínima dé el valor máximo (28) y la masa máxima dé el valor mínimo (1)
+	var pitch = 28 - ((mass - min_mass) / (max_mass - min_mass)) * (28 - 1)
+	print(pitch)
+	# Redondeamos el resultado y lo convertimos en entero
+	return int(round(pitch))
+
+	
+func random_pitch() -> float:
+	var random_index = randi() % 28 + 1 # Número aleatorio entre 1 y 28
+	return random_index
+	
+func calculate_timber(radius: float, min_radius: float = 1000.0, max_radius: float = 10000000.0) -> float:
+	# Limitar la masa para que no salga del rango definido
+	radius = max(min_radius, min(radius, max_radius))
+	
+	# Calcular el valor inversamente proporcional entre 1 y 28
+	# Queremos que la masa mínima dé el valor máximo (28) y la masa máxima dé el valor mínimo (1)
+	var timber = sounds_array.size() - ((radius - min_radius) / (max_radius - min_radius)) * (sounds_array.size() - 1)
+	print(timber)
+	# Redondeamos el resultado y lo convertimos en entero
+	return int(round(timber) - 1)
+
+func temperature_to_sound(temperature: float, audio: AudioStreamPlayer3D):
+	if(temperature > 100000.0):
+		audio.bus = "High Bus"
+	elif(temperature > 10000.0):
+		audio.bus = "Mid Bus"
+	else:
+		audio.bus = "Low Bus"
+	
+func random_timber()->int:
+	var random_index = randi() % sounds_array.size() + 0
+	return random_index
+
+func get_audio_file(pitch: float, library: int) -> String:
+	# Assuming the audio files are named based on the pitch value
+	print("TImbre: ", sounds_array[library])
+	#return "res://Sounds/ritmicos/" + str(pitch) + ".wav"
+	return "res://Sounds/" + sounds_array[library] + "/" + str(pitch) + ".wav"
+	
+func get_audio_file_prueba() -> String:
+	# Assuming the audio files are named based on the pitch value
+	var random_index = randi() % 5 + 1
+	return "res://Sounds/melodias/" + str(random_index) + ".wav"
+
+func play_audio(audio: AudioStreamPlayer3D):
+	if active_sounds < max_sounds:
+		audio.play()
+		active_sounds += 1
+
+#Fixed Sounds
+var audio_player_hyperdrive: AudioStreamPlayer
+var audio_player_waves: AudioStreamPlayer
+
 # PLANET COLOR DATA #
 const V_0 = 3.63e-20
 const Ks_0 = 4.283e-21
@@ -35,7 +99,23 @@ func _ready():
 	root.name = "StarSystems"
 	add_child(root)
 	
-	_change_system("11 Com")
+	#perdon por esto juan
+	audio_player_hyperdrive = AudioStreamPlayer.new()
+	add_child(audio_player_hyperdrive)
+	audio_player_hyperdrive.stream = load("res://Sounds/ambient/hyperdrive.mp3")
+	audio_player_hyperdrive.bus = "MainBus"  # Asignar el bus
+	#audio_player_hyperdrive.volume_db = -50  # Ajustar volumen (-10 dB)
+	audio_player_hyperdrive.play()
+
+	audio_player_waves = AudioStreamPlayer.new()
+	add_child(audio_player_waves)
+	audio_player_waves.stream = load("res://Sounds/ambient/waves.mp3")
+	audio_player_waves.bus = "MainBus"  # Asignar el bus
+	#audio_player_waves.volume_db = -50  # Ajustar volumen (-10 dB)
+	audio_player_waves.play()
+	
+	#_change_system("11 Com")
+	_change_system("24 Sex")
 
 func _change_system(new_system: String):
 	chosen_system = new_system
@@ -56,9 +136,14 @@ func _change_system(new_system: String):
 		system_node.add_child(body)
 		
 		if base_body is Star3D:
+			#play_audio(body.star_audio)
+			
+			for audio_player in body.get_children():
+				if audio_player is AudioStreamPlayer3D:
+					audio_player.play()
+			
 			print("estrella")
 			body.global_position = body.star_position
-			
 			$Camera3D.global_position = body.global_position
 			$Camera3D.global_position.z += 5
 			$Camera3D.look_at(body.global_position)
@@ -69,6 +154,9 @@ func _change_system(new_system: String):
 			var distance = randf_range(body.orbit_radius / 2, body.orbit_radius) * 2.5
 			var end_pos = center + direction * distance
 			body.global_position = end_pos
+			for audio_player in body.get_children():
+				if audio_player is AudioStreamPlayer3D:
+					audio_player.play()
 
 func _start_systems():
 	var stars = preload("res://Data/StarData.csv")
@@ -89,7 +177,18 @@ func _start_systems():
 			
 			var star_material: ShaderMaterial = ShaderMaterial.new()
 			star_material.shader = star_shader
-			
+			# Create the audio player
+			var star_audio = AudioStreamPlayer3D.new()
+			var star_pitch = calculate_pitch(float(star.st_mass))
+			var random_pitch = random_pitch()
+			##var star_timber = calculate_timber(float(star.st_rad))
+			var star_timber = random_timber()
+			star_audio.stream = load(get_audio_file(star_pitch, star_timber)) # Assign the .wav file based on pitch
+			star_audio.autoplay = false # Disable autoplay
+			star_audio.max_distance = 2000 # Set max audible distance
+			star_audio.volume_db = -10 # Lower the volume
+			star_audio.unit_size = 1.0 # Control sound attenuation
+			star_node.add_child(star_audio)
 			var star_color: Color = Color("EDEFCA")
 			
 			if star.st_spectype.containsn("M"):
@@ -130,7 +229,17 @@ func _start_systems():
 			var mesh = MeshInstance3D.new()
 			mesh.mesh = SphereMesh.new()
 			planet_node.add_child(mesh)
-			
+			var planet_audio = AudioStreamPlayer3D.new()
+			var planet_pitch = calculate_pitch(float(planet.pl_bmasse))
+			##var planet_timber = calculate_timber(float(planet.pl_radj))
+			var planet_timber = random_timber()
+			var random_pitch = random_pitch()
+			planet_audio.stream = load(get_audio_file(planet_pitch, planet_timber)) # Assign the .wav file based on pitch
+			planet_audio.autoplay = false # Disable autoplay
+			planet_audio.max_distance = 2000 # Set max audible distance
+			planet_audio.volume_db = -10 # Lower the volume
+			planet_audio.unit_size = 1.0 # Control sound attenuation
+			planet_node.add_child(planet_audio)
 			planet_node.host_star = planet.hostname
 			planet_node.orbit_radius = float(planet.pl_orbsmax)
 			
